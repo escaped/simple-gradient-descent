8c048ea6782a84bd64222f51bab7aa9819230c84
Alexander Frenzel <alex@relatedworks.com>
2017-07-18 17:37:04 +0200

    add cli

--- gradient.py
+++ gradient.py
@@ -1,11 +1,13 @@
-import math
-
+import click
 import matplotlib.cm as cm
 import matplotlib.pyplot as plt
 import numpy as np
 
 
 def error(X, Y_true, w, b):
+    """
+    Calculates the error for the given weight and bias for each point.
+    """
     err = None
     for x, y_true in zip(X, Y_true):
         y = sigmoid(w * x + b)
@@ -15,19 +17,32 @@ def error(X, Y_true, w, b):
             err += 1/2 * (y - y_true) ** 2
     return err
 
+
 def sigmoid(value):
+    """
+    Implements the sigmoid activation function.
+    """
     return 1 / (1 + np.exp(-value))
 
 
 def sigmoid_1(value):
+    """
+    Implements the derivation of the sigmoid function.
+    """
     return sigmoid(value) * (1 - sigmoid(value))
 
 
 def neuron(input, weight, bias):
+    """
+    Calulates the output of an artificial neuron.
+    """
     return sigmoid(weight * input + bias)
 
 
 def plot_surface(X, Y_true):
+    """
+    Plots the error surface.
+    """
     n = 256
     w = np.linspace(-3.0, 3.0, n)
     b = np.linspace(-3.0, 3.0, n)
@@ -46,6 +61,9 @@ def plot_surface(X, Y_true):
 
 
 def plot_sigma(X, Y, w, b):
+    """
+    Plots the learned function of our artifical neuron and the example points.
+    """
     plt.figure()
     n = 256
     X_sigma = np.linspace(-5.0, 5.0, n)
@@ -55,16 +73,24 @@ def plot_sigma(X, Y, w, b):
         plt.plot(x, y, marker='o', color='red')
 
 
-if __name__ == '__main__':
-    threshold = 0.0001
-    lr = 0.1
+@click.command()
+@click.option('--threshold', default=0.0001, type=click.FLOAT,
+              help="Train until the error reaches this threshold.")
+@click.option('--lr', default=0.1, type=click.FLOAT, help="Learning rate")
+@click.option('--initial_weight', default=0.5, type=click.FLOAT)
+@click.option('--initial_bias', default=0.3, type=click.FLOAT)
+@click.option('--point', nargs=2,
+              type=click.Tuple([float, click.FloatRange(0, 1, clamp=True)]),
+              multiple=True, required=True,
+              help="Supply at least one data point to approximate.")
+def train(threshold, lr, initial_weight, initial_bias, point):
+    X = np.asarray([x for x, _ in point])
+    Y_true = np.asarray([y for _, y in point])
+    print(f'Input data: {X}')
+    print(f'Expected data: {Y_true}')
 
-    X = np.asarray([1, -3.5])
-    Y_true = np.asarray([0.6, 0.5])
-    print(f'expected: {Y_true}')
-
-    w = 0.5  # initial weight
-    b = 0.3  # initial bias
+    w = initial_weight
+    b = initial_bias
 
     plot_sigma(X, Y_true, w, b)
     plot_surface(X, Y_true)
@@ -74,6 +100,8 @@ if __name__ == '__main__':
     count = 0
     while err > threshold:
         print(f'{count}| error: {err}, y: {Y}, y_true: {Y_true}, w: {w}), b: {b}')
+
+        # calculate the gradient of w and b
         gradient_w = gradient_b = 0
         for x, y, y_true in zip(X, Y, Y_true):
             z = w * x + b
@@ -81,13 +109,13 @@ if __name__ == '__main__':
             gradient_b += (y - y_true) * sigmoid_1(z)
         delta_w = -lr * gradient_w
         delta_b = -lr * gradient_b
-        print(f'{count}| gradient_w: {gradient_w}, gradient_b: {gradient_b}')
-        print(f'{count}| dw: {delta_w}, db: {delta_b}')
 
+        # plot movement through the error space
         plt.quiver(w, b, delta_w, delta_b, color='red',
                    width=0.005, scale_units='xy',
                    scale=1.0, angles='xy', zorder=2)
 
+        # update w and b
         w += delta_w
         b += delta_b
         Y = neuron(X, w, b)
@@ -97,3 +125,7 @@ if __name__ == '__main__':
 
     plot_sigma(X, Y_true, w, b)
     plt.show()
+
+
+if __name__ == '__main__':
+    train()
